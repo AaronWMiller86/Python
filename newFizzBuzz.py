cc0260632b396bdf64f6417f6930434ac08d9e34
Aaron W Miller <aaronwmiller86@gmail.com>
2024-03-19 11:43:13 -0400

    Added newFizzBuzz.py

---
+++ newFizzBuzz.py
@@ -0,0 +1,10 @@
+new_range = range(1,101)
+for number in new_range:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
