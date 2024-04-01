e57d5d98dd550c541b5faf237c7f2fe32f0dd2d4
Aaron W Miller <aaronwmiller86@gmail.com>
2024-04-01 15:57:59 -0400

    Added updated Ceasar.py

---
+++ Ceasar.py
@@ -0,0 +1,16 @@
+def ceasar(text, amount):
+    correction_amount = 26 - amount
+    result = ""
+    for letter in text:
+        if letter.isalpha():
+            if chr(ord(letter) + amount).isalpha():
+                unicode = ord(letter) + amount
+                result += chr(unicode)
+            else:
+                unicode = ord(letter) - correction_amount
+                result += chr(unicode)
+        else:
+            result += letter
+    return "The encrypted sentence is: " + result.lower()
+
+print(ceasar(input("Please enter a sentence: "), input("Please enter cipher stepping amount: ")))
